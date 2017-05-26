409b0c2a1a33518b7ecd94ac78a1509e920cc510
zhangxueyangjuxie <1512935295@qq.com>
2017-05-26 10:53:16 +0800

    commit

---
+++ 01_GMM-UBM_system_with_ALIZE3.0/get_all_utta_path.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#descript : get dataset base information
+#author : xueyang
+#data : 2017-05-08
+
+#create path file
+if [ -f "data/all_utta_path" ]; then
+	rm data/data.lst
+fi
+touch data/data.lst
+
+#iterator find .wav files
+function get_wav()
+{
+	for element in `ls $1`
+	do
+		dir_or_file=$1"/"$element
+		if [ -d $dir_or_file ]; then
+			get_wav $dir_or_file
+		else
+			if [ "${dir_or_file##*.}"x = "wav"x ]; then
+				echo $dir_or_file >> data/data.lst
+			fi
+		fi
+	done	
+}
+get_wav $1
